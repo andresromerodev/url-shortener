c66e0ed4bded42da16691f18d1239af4ef7d24c4
andresromeroh <andresromeroh.cr@gmail.com>
2023-11-05 14:42:17 -0600

    feat(api): initial bootstrapping and shorten endpoint

---
+++ api/go.mod
@@ -0,0 +1,25 @@
+module github.com/andresromeroh/url-shortener/api
+
+go 1.20
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/gofiber/fiber/v2 v2.50.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/iancoleman/strcase v0.0.0-20190422225806-e506e3ef7365 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/matoous/go-nanoid/v2 v2.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+	github.com/steebchen/prisma-client-go v0.27.1 // indirect
+	github.com/takuoki/gocase v1.0.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.50.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
---
+++ api/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/andresromeroh/url-shortener/api/db"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	gonanoid "github.com/matoous/go-nanoid/v2"
+)
+
+type CreateUrlReq struct {
+	LongURL string `json:"longUrl"`
+}
+
+func getPrismaClient() (*db.PrismaClient, context.Context, error) {
+	client := db.NewClient()
+
+	if err := client.Prisma.Connect(); err != nil {
+		return nil, nil, err
+	}
+
+	bg := context.Background()
+
+	return client, bg, nil
+}
+
+func health(c *fiber.Ctx) error {
+	response := fiber.Map{
+		"status": "ok",
+	}
+	return c.JSON(response)
+}
+
+func shorten(c *fiber.Ctx) error {
+	request := new(CreateUrlReq)
+
+	if err := c.BodyParser(request); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": "Invalid JSON",
+		})
+	}
+
+	nanoid, _ := gonanoid.New()
+	shortUrl := os.Getenv("BASE_URL") + nanoid
+
+	client, bg, _ := getPrismaClient()
+
+	createdUrl, err := client.URL.CreateOne(
+		db.URL.ID.Set(uuid.New().String()),
+		db.URL.ShortURL.Set(shortUrl),
+		db.URL.LongURL.Set(request.LongURL),
+	).Exec(bg)
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(createdUrl)
+}
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	app := fiber.New()
+
+	app.Get("/api/v1/health", health)
+	app.Post("/api/v1/shorten", shorten)
+
+	log.Fatal(app.Listen("127.0.0.1:3000"))
+}
