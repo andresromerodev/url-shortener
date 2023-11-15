ba66014b7586d36ef101d146aab3a5375f4779a2
andresromeroh <andresromeroh.cr@gmail.com>
2023-11-15 09:56:09 -0600

    feat(ui): allow to shorten a url from ui

--- ui/app.vue
+++ ui/app.vue
@@ -1,55 +1,38 @@
 <script setup>
-const value = ref('')
+const url = ref('')
 
-const people = [{
-  id: 1,
-  name: 'Lindsay Walton',
-  title: 'Front-end Developer',
-  email: 'lindsay.walton@example.com',
-  role: 'Member'
-}, {
-  id: 2,
-  name: 'Courtney Henry',
-  title: 'Designer',
-  email: 'courtney.henry@example.com',
-  role: 'Admin'
-}, {
-  id: 3,
-  name: 'Tom Cook',
-  title: 'Director of Product',
-  email: 'tom.cook@example.com',
-  role: 'Member'
-}, {
-  id: 4,
-  name: 'Whitney Francis',
-  title: 'Copywriter',
-  email: 'whitney.francis@example.com',
-  role: 'Admin'
-}, {
-  id: 5,
-  name: 'Leonard Krasner',
-  title: 'Senior Designer',
-  email: 'leonard.krasner@example.com',
-  role: 'Owner'
-}, {
-  id: 6,
-  name: 'Floyd Miles',
-  title: 'Principal Designer',
-  email: 'floyd.miles@example.com',
-  role: 'Member'
-}]
+function shorten() {
+  const response = $fetch('http://localhost:5000/api/v1/shorten', {
+    method: 'POST',
+    body: { longUrl: url.value }
+  })
+  console.log(JSON.stringify(response))
+}
+
 </script>
 
 <template>
   <UContainer>
     <br />
     <h1>URL Shortener</h1>
-    <UInput v-model="value" size="sm" />
+    <UInput v-model="url" size="sm" />
     <br />
-    <UButton>Shorten</UButton>
+    <UButton @click="shorten">Shorten</UButton>
     <UButton style="margin-left: 4px;" color="red" variant="solid">Clear</UButton>
     <br />
-    <UTable :rows="people" />
+    <br />
+    <UCard>
+      <ULink
+        class="h-32"
+        to="/elements/link"
+        active-class="text-primary"
+        inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+      >
+        https://google.com
+    </ULink>
+    </UCard>
+    <br />
+    <UTable :rows="[]" />
     <br />
   </UContainer>
 </template>
