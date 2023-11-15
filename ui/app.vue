ee8c620e7cf12ec6a19f631c3466b6c150f85b64
andresromeroh <andresromeroh.cr@gmail.com>
2023-11-15 13:50:41 -0600

    fix(ui): add url to link display

--- ui/app.vue
+++ ui/app.vue
@@ -45,7 +45,7 @@ function clear() {
     <UCard>
       <ULink
         class="h-32"
-        to="/elements/link"
+        :to="shortUrl"
         active-class="text-primary"
         inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
       >
